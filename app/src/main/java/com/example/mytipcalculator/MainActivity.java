8768872a188de758a68913d28de66e2ff86f7e4d
toivan <33359118+toivan@users.noreply.github.com>
2019-02-10 20:44:33 +0300

    10-02-19 20:41

---
+++ app/src/main/java/com/example/mytipcalculator/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.mytipcalculator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
